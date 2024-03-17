be7b0d59cac3e8992900fb763671a39d18e6332c
aadhil-t <aadhilshajith03@gmail.com>
2024-03-18 03:47:31 +0530

    Updated on HubAdmin Chart

--- src/components/HubAdmin/Dashboard.jsx
+++ src/components/HubAdmin/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { DashBoardData } from "../../Api/HubAdminApi";
 import { AgChartsReact } from "ag-charts-react";
 import moment from "moment";
+import { FaBook } from "react-icons/fa";
 
 export default function Dashboard() {
   const [options, setOptions] = useState({
@@ -35,7 +36,9 @@ export default function Dashboard() {
                 propertyBookings: response.data.PresentUserBookedData,
               },
               {
-                year: moment(response.data.PreviousMonthStartDate).format("MMM"),
+                year: moment(response.data.PreviousMonthStartDate).format(
+                  "MMM"
+                ),
                 avgTemp: 6.3,
                 propertyBookings: response.data.PreviousUserBookedData,
               },
@@ -57,10 +60,28 @@ export default function Dashboard() {
 
   return (
     <div
-      className="flex justify-center items-center h-[55rem]"
+      className="flex flex-col h-screen"
       style={{ backgroundColor: "#1B4965" }}
     >
-      <div>
+      <div className="flex justify-evenly">
+        <>
+        <div className="flex gap-96">
+          <div className="mx-6 my-5 text-5xl flex flex-row justify-evenly items-center bg-white w-32 h-24 rounded-xl ">
+            <FaBook />
+            <div className="bg-yellow-700">jjjj</div>
+          </div>
+          <div className="mx-6 my-5 text-5xl flex flex-row justify-evenly items-center bg-white w-32 h-24 rounded-xl">
+            <FaBook />
+            <div className="bg-yellow-700">jjjj</div>
+          </div>
+          <div className="mx-6 my-5 text-5xl flex flex-row justify-evenly items-center bg-white w-32 h-24 rounded-xl">
+            <FaBook />
+            <div className="bg-yellow-700">jjjj</div>
+          </div>
+          </div>
+        </>
+      </div>
+      <div className="mx-6 mt-5 w-[40rem]">
         <AgChartsReact options={options} />
       </div>
     </div>
